fe6d63ddaef949fc3f418637baae807a697535ee
Filipe Cancellier da Costa <filipecancelliercosta@gmail.com>
2022-03-31 16:57:08 +0100

    novo

--- index.js
+++ index.js
@@ -9,9 +9,15 @@ button.addEventListener("click", () => {
   let password = document.querySelector("input").value
   console.log(password.value)
   if (password === bdPassword) {
-    document.querySelector(".errorMessage").innerHTML = ""
-    console.log("acertou!!")
+    document.querySelector(".errorMessage").style.color = 'yellow'
+    document.querySelector(".errorMessage").innerHTML = "Right Password! Loading..."
+    let a = () => {
+        document.querySelector(".hidedDiv").style.display = "flex"
+      }
+      setInterval(a, 3000)
+    
   } else {
+    document.querySelector(".errorMessage").style.color = 'red'
     document.querySelector(".errorMessage").innerHTML =
       "Wrong Password! Try again"
     let a = () => {
